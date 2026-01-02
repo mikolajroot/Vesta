66e5cbaf3e316848a0c6785a71793788812ce45a
mikolajroot <mikolaj150878@gmail.com>
2026-01-02 16:49:07 +0100

    Added initial Prisma setup and environment configuration

--- vesta/apps/vesta/src/app/app.tsx
+++ vesta/apps/vesta/src/app/app.tsx
@@ -6,9 +6,7 @@ import { Route, Routes, Link } from 'react-router-dom';
 export function App() {
   return (
     <div>
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
+
       <br />
       <hr />
       <br />
