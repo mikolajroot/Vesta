4fa83c0526b56bb688bd66f7c121ef9066b9f012
mikolajroot <mikolaj150878@gmail.com>
2026-01-02 20:18:40 +0100

    Added PostgreSQL support and updated ROOM model in Prisma schema

--- vesta/api/src/main.ts
+++ vesta/api/src/main.ts
@@ -7,6 +7,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
