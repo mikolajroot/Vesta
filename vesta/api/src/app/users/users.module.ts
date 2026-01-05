e3b567f890f52b7ac438b01259e5ab61df071601
mikolajroot <mikolaj150878@gmail.com>
2026-01-05 18:22:09 +0100

    little fix

--- vesta/api/src/app/users/users.module.ts
+++ vesta/api/src/app/users/users.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 @Module({
   providers: [UsersService, PrismaService],
-  controllers: [UsersController],
   exports: [UsersService],
 })
 export class UsersModule {}
