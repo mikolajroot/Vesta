f72a2193c279d8a7e4c9fdc7f652bbeb04f07b3c
mikolajroot <mikolaj150878@gmail.com>
2026-01-11 13:03:23 +0100

    Added getAllHomes method to homes service

--- vesta/api/src/app/homes/homes.service.ts
+++ vesta/api/src/app/homes/homes.service.ts
@@ -4,13 +4,15 @@ import { CreateHomeDto } from './dto/create-home.dto';
 import { Home } from '../../generated/prisma/client';
 @Injectable()
 export class HomesService {
-  constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-  async create(createHomeDto:CreateHomeDto): Promise<{ message: string }>{
-    await this.prisma.home.create({ data: createHomeDto})
+  async create(createHomeDto: CreateHomeDto): Promise<{ message: string }> {
+    await this.prisma.home.create({ data: createHomeDto });
 
-    return { message: "Home creates successfully"}
+    return { message: 'Home creates successfully' };
   }
 
-
+  async getAllHomes(userId: number): Promise<Home[] | null> {
+    return this.prisma.home.findMany({ where: { users_id: { has: userId } } });
+  }
 }
