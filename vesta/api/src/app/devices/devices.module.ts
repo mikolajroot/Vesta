d2a22f44a7a41359fdf033a07b3a974109654d09
mikolajroot <mikolaj150878@gmail.com>
2026-01-28 14:02:51 +0100

    Added DevicesGateway to providers and exports in DevicesModule

--- vesta/api/src/app/devices/devices.module.ts
+++ vesta/api/src/app/devices/devices.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DevicesService } from './devices.service';
 import { DevicesController } from './devices.controller';
+import { DevicesGateway } from './devices.gateway';
 import { PrismaService } from '../../prisma.service';
 
 @Module({
   controllers: [DevicesController],
-  providers: [DevicesService,PrismaService],
+  providers: [DevicesService, PrismaService, DevicesGateway],
+  exports: [DevicesService, DevicesGateway],
 })
 export class DevicesModule {}
