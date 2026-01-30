130bccb14352247f30b3902002579fc136bed66e
mikolajroot <mikolaj150878@gmail.com>
2026-01-30 15:38:57 +0100

    Added MqttService to DevicesModule and configured MQTT initialization

--- vesta/api/src/app/devices/devices.module.ts
+++ vesta/api/src/app/devices/devices.module.ts
@@ -2,11 +2,25 @@ import { Module } from '@nestjs/common';
 import { DevicesService } from './devices.service';
 import { DevicesController } from './devices.controller';
 import { DevicesGateway } from './devices.gateway';
+import { MqttService } from './mqtt.service';
 import { PrismaService } from '../../prisma.service';
 
 @Module({
   controllers: [DevicesController],
-  providers: [DevicesService, PrismaService, DevicesGateway],
-  exports: [DevicesService, DevicesGateway],
+  providers: [
+    DevicesService,
+    PrismaService,
+    DevicesGateway,
+    MqttService,
+    {
+      provide: 'MQTT_INIT',
+      useFactory: (devicesService: DevicesService, mqttService: MqttService) => {
+        devicesService.setMqttService(mqttService);
+        return null;
+      },
+      inject: [DevicesService, MqttService],
+    },
+  ],
+  exports: [DevicesService, DevicesGateway, MqttService],
 })
 export class DevicesModule {}
